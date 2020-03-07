4199187ca04e5f9dc149e8d20d990a236daaeb69
Suzin <patrycjusz.sosnowski@gmail.com>
2020-03-07 23:30:44 +0100

    migrations examples

--- database/migrations/2020_03_06_200659_add_column_items.php
+++ database/migrations/2020_03_06_200659_add_column_items.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateItemsTable extends Migration
+class AddColumnItems extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateItemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('items', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->text('text')->nullable();
-            $table->integer('damage');
-            $table->integer('durability');
-            $table->timestamps();
+        Schema::table('items', function (Blueprint $table) {
+
+                $table->string('test_column')->default('default')->change();
         });
     }
 
@@ -30,6 +26,8 @@ class CreateItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('items');
+        Schema::table('items', function (Blueprint $table) {
+            //
+        });
     }
 }
--- database/migrations/2020_03_07_163014_create_profiles_table.php
+++ database/migrations/2020_03_07_163014_create_profiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateItemsTable extends Migration
+class CreateProfilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateItemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('items', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('text')->nullable();
-            $table->integer('damage');
-            $table->integer('durability');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->index('user_id');
         });
     }
 
@@ -30,6 +29,6 @@ class CreateItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('items');
+        Schema::dropIfExists('profiles');
     }
 }
